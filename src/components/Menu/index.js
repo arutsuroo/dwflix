9b807f6f08412c7cb63b803972883f7605dffc9f
Arthur Oliveira <arthur2109.vieira@gmail.com>
2020-07-27 22:08:23 -0300

    First Day: Menu

---
+++ src/components/Menu/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Logo from '../../assets/img/logo.png';
+import './Menu.css';
+import Button from '../Button';
+
+function Menu() {
+    return (
+        <nav className="Menu">
+          <a href="/">
+            <img className="Logo" src={Logo} alt="DWflix logo"/>
+          </a>
+
+        <Button as="a" className="ButtonLink" href="/">
+            Novo episódio
+        </Button>
+
+        </nav>
+    );
+}
+
+export default Menu;
